7869c0765bc2729f2c815376404add7c23d2fbf8
PatrykMa <wiatraczekk@gmail.com>
2019-06-27 21:39:26 +0200

    Add drawer

--- app/src/main/java/com/example/canvastest/MainActivity.kt
+++ app/src/main/java/com/example/canvastest/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.canvastest
 import android.graphics.Point
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.view.MenuItem
 import com.example.canvastest.Elements.*
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +18,25 @@ class MainActivity : AppCompatActivity() {
         circuitView.addElement(FlowSource(Point(100, 100), Point(300, 100)))
         circuitView.addElement(Resistor(Point(100, 300), Point(300, 300)))
         circuitView.addElement(TensionSource(Point(100,500),Point(300,500)))
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_resistor -> {
+                circuitView.addElement(Resistor(Point(100, 100), Point(300, 100)))
+            }
+            R.id.nav_flow -> {
+                circuitView.addElement(FlowSource(Point(100, 100), Point(300, 100)))
+            }
+            R.id.nav_tension -> {
+                circuitView.addElement(TensionSource(Point(100,100),Point(300,100)))
+            }
+        }
+        circuitView.invalidate()
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
